92796d0cbe50e495fbe4d96296f187e47bdf88f4
Phong Phạm Hùng <hungphong.canhan@gmail.com>
2024-05-05 15:42:26 +0700

    start project

---
+++ src/main/java/com/example/WebBanVe/WebBanVeApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebBanVe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanVeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBanVeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WebBanVe/WebBanVeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebBanVe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanVeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
